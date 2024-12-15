857c2b026a4b54cc63142675955d099c6f6ee6b4
Itismeusee1235 <pradyumnrayasam22@gmail.com>
2024-12-15 17:16:48 +0530

    did somestuff with eww

--- eww/launch.sh
+++ eww/launch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-ewwDir=/home/fenrir/External\ Repos/eww/target/release/eww
-
-
 pkill eww
-"$ewwDir" daemon --config /home/fenrir/.config/i3/eww/
-"$ewwDir" open $1 --config /home/fenrir/.config/i3/eww/
+eww daemon --config /home/fenrir/.config/i3/eww/eww-bar &
+eww open $1 --config /home/fenrir/.config/i3/eww/ --debug
 
 
 
